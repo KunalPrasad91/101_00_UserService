361e5fb38a4f6d478c50b3407e5af3ce89da7fbf
Kunal <prasadkunal0691@gmail.com>
2024-01-19 18:52:21 +0530

    User Service First Commit

---
+++ src/main/java/com/scaler/UserServiceJan24/UserServiceJan24Application.java
@@ -0,0 +1,13 @@
+package com.scaler.UserServiceJan24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceJan24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceJan24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/scaler/UserServiceJan24/UserServiceJan24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.UserServiceJan24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceJan24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
